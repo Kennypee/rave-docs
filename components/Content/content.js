50155f9ecc42c3801cf26d26e3117e7cd1b2893c
adexot <adekunleoseni47@gmail.com>
2018-03-31 15:05:04 +0100

    added content component to the doc page

--- components/Content/content.js
+++ components/Content/content.js
@@ -2,7 +2,19 @@ import style from './content.scss';
 
 const Content = () => (
     <div className={style.content}>
-        
+        <div className={style.nav}>
+            <form action="" method="" className={style.searchForm}>
+                <input type="text" placeholder="Search Documentation"/>
+            </form>
+
+            <ul className={style.pullRight}>
+                <li>Dashboard</li>
+                <li> 
+                    <img src="/static/avatar.svg" alt="christian" />
+                    Christian
+                </li>
+            </ul>
+        </div>
     </div>
 );
 
