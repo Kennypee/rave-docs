db6b39caedc08efd3af942138b90295601a30cdc
chiefoleka <valentinenwakibeya@gmail.com>
2018-04-20 19:49:18 +0100

    Created the pages directory for all guides using designed for ios

---
+++ pages/ios/sections/index.js
@@ -0,0 +1,10 @@
+import Layout from 'components/Layout';
+
+export default class Web extends React.Component {
+	render() {
+        return (
+            <Layout>
+            </Layout>
+        );
+    }
+}
