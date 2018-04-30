e4f57bbb13643b46bbb2d3fc987a1fb5b2a77eeb
chiefoleka <valentinenwakibeya@gmail.com>
2018-04-30 14:55:04 +0100

    Added the component switch for web section

--- pages/web/sections/index.js
+++ pages/web/sections/index.js
@@ -1,37 +1,53 @@
 import Layout from 'components/Layout'
+import Sidebar from 'components/InternalSidebar';
 import style from 'content/style.scss';
 
 export default class Section extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { component: Default };
+        this.state = { 
+            component: Default,
+            routes : {}
+        };
     }
     componentDidMount(){
         this.props.url.query.section
         switch (this.props.url.query.section) {
             case "react":
                 import("content/web/react").then(module => this.setState({ component: module.default }))
+                import("content/web/react/routes.js").then(module => this.setState({ routes: module.default }))
                 break;
             case "vue":
                 import("content/web/vue").then(module => this.setState({ component: module.default }))
+                import("content/web/vue/routes.js").then(module => this.setState({ routes: module.default }))
                 break;
             case "angular":
                 import("content/web/angular").then(module => this.setState({ component: module.default }))
+                import("content/web/angular/routes.js").then(module => this.setState({ routes: module.default }))
                 break;
             case "node":
                 import("content/web/node").then(module => this.setState({ component: module.default }))
+                import("content/web/node/routes.js").then(module => this.setState({ routes: module.default }))
                 break;
             case "php":
                 import("content/web/php").then(module => this.setState({ component: module.default }))
+                import("content/web/php/routes.js").then(module => this.setState({ routes: module.default }))
                 break;
             case "go":
                 import("content/web/go").then(module => this.setState({ component: module.default }))
+                import("content/web/go/routes.js").then(module => this.setState({ routes: module.default }))
                 break;
-            case "woocommerce":
-                import("content/web/woocommerce").then(module => this.setState({ component: module.default }))
+            case "ruby":
+                import("content/web/ruby").then(module => this.setState({ component: module.default }))
+                import("content/web/ruby/routes.js").then(module => this.setState({ routes: module.default }))
                 break;
-            case "wordpress":
-                import("content/web/wordpress").then(module => this.setState({ component: module.default }))
+            case "python":
+                import("content/web/python").then(module => this.setState({ component: module.default }))
+                import("content/web/python/routes.js").then(module => this.setState({ routes: module.default }))
+                break;
+            case "csharp":
+                import("content/web/csharp").then(module => this.setState({ component: module.default }))
+                import("content/web/csharp/routes.js").then(module => this.setState({ routes: module.default }))
                 break;
             default:
                 break;
@@ -39,9 +55,10 @@ export default class Section extends React.Component {
     }
 
 	render() {
-        const { component: Component } = this.state;
+        const { component: Component} = this.state;
         let props = {
-            content:<div className={style.markdown}><Component/></div>
+            content:<Component/>,
+            sidebar:<Sidebar routes={this.state.routes} />
         }
         return (
             <Layout {...props}>
