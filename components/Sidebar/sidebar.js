d4507cc7f2fa2dae4270d7400fa4de070cc3e423
chiefoleka <valentinenwakibeya@gmail.com>
2018-05-08 11:40:43 +0100

    Modified it to take in external route info as json

--- components/Sidebar/sidebar.js
+++ components/Sidebar/sidebar.js
@@ -1,37 +1,56 @@
 import style from './sidebar.scss';
 
-const Sidebar = () => (
-    <div className={style.sidebar}>
-        <div className={style.brand}>
-            <a href="">
-                <img src="/static/rave.svg" alt=""/>
-            </a>
-        </div>
-        <div className={style.menu}>
-            <a href="" className={style.back}>
-                <img src="/static/chevron-left.svg" alt=""/>
-                Back
-            </a>
-            <header className={style.title}>NodeJS</header>
-            <ul className={style.list}>
-                <li>Getting Started</li>
-                <li> 
-                    <a href="">Setting up on Rave</a>
-                    <ul className={style.subList}>
-                        <li>Prerequisite</li>
-                        <li>Rave parameters</li>
-                        <li>Checksum</li>
-                        <li>Hosted payment pages</li>
-                        <li>Verify payments</li>
-                    </ul>
-                </li>
-                <li>Card charge</li>
-                <li>Bank account charge</li>
-                <li>Preauthorisation</li>
-                <li>Transaction status</li>
-            </ul>
-        </div>
-    </div>
-);
+export default class Sidebar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { fixed: false};
+    }
+    componentDidMount(){
+        window.addEventListener('scroll', (event) => {
+            let fixed = false
+            if(window.scrollY > 60){
+                this.setState({fixed: true})
+            }else{
+                this.setState({fixed: false})
+            }
+            
+        })
+    }
 
-export default Sidebar;
+    render() {
+        return (
+            <div className={style.sidebar}>
+                <div className={style.brand}>
+                    <a href="">
+                        <img src="/static/rave.svg" alt=""/>
+                    </a>
+                </div>
+                <div className={style.menu}>
+                    {/*<a href="" className={style.back}>
+                        <img src="/static/chevron-left.svg" alt=""/>
+                        Back
+                    </a>
+                    <header className={style.title}>NodeJS</header>*/}
+                    <ul className={style.list}>
+                        {
+                            Object.keys(this.props.routes).map((topLevel, index) => (
+                                <li key={index}>
+                                    <span className={style.title}>{topLevel}</span>
+                                    <ul className={style.subList}>
+                                        {
+                                            this.props.routes[topLevel].map((route, index) => (
+                                                <li key={index}>
+                                                    <a href={route.url}>{route.title}</a>
+                                                </li>
+                                            ))
+                                        } 
+                                    </ul> 
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+            </div> 
+        );
+    }
+}
