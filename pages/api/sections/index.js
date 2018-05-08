d56dbc6353d18ba7500823b0e29e76189565ae4f
chiefoleka <valentinenwakibeya@gmail.com>
2018-05-08 12:20:37 +0100

    added content for API

--- pages/api/sections/index.js
+++ pages/api/sections/index.js
@@ -1,3 +1,49 @@
-import Doc from './doc';
+import React from 'react';
+import style from './style.scss';
+import Head from "components/head";
+import Sidebar from 'components/Sidebar';
+import Content from 'components/Content'; 
+import Routes from 'content/api/routes.js';
 
-export default Doc;
+export default class Section extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = { component: Default };
+    }
+    componentDidMount(){
+        this.props.url.query.section
+        switch (this.props.url.query.section) {
+            case "api-keys":
+                import("content/api/api-keys").then(module => this.setState({ component: module.default})) 
+                break;
+            case "home":         
+                this.setState({ component: Home})
+                break;
+            default:  
+                break; 
+        }
+    }
+    render(){
+        const { component: Component } = this.state;
+
+        return(
+            <React.Fragment>
+            <Head />
+            <div className={style.doc}>
+                <Sidebar routes={Routes}/>
+                <Content>  
+                    <Component />        
+                </Content>
+            </div>
+            </React.Fragment>
+        );
+    }
+}
+const Home = () => (
+    <h2>Rave API Documentation.</h2>
+)
+const Default = () => (
+    <div style={{margin:'0 auto',display:'table'}}>
+        <img src="/static/loading.svg" alt="" />
+    </div>
+)
